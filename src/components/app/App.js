11e28b6bc92b41d085230d97ac3ef91939cd0516
viniciusrma <viniciusrma.adv@gmail.com>
2020-07-30 14:27:01 -0300

    stylish improvement

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,7 +6,7 @@ import SearchBar from '../searchbar/SearchBar';
 function App() {
   return (
     <div className="App">
-      <h1><img src="https://i.ibb.co/zhtG3mx/Logo.png" alt="Logo of hunger of what" width="40" height="40" padding="5"/>Hunger of What?!</h1>
+      <h1><img id="logo" src="https://i.ibb.co/zhtG3mx/Logo.png" alt="Logo of hunger of what"/>Hunger of What?!</h1>
       <SearchBar />
       <BusinessList />
     </div>
--- src/components/business/Business.js
+++ src/components/business/Business.js
@@ -20,9 +20,9 @@ class Business extends React.Component {
         <div className="image-container">
           <img src={business.imageSrc}  alt='' />
         </div>
-        <h2>{business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
+            <h2>{business.name}</h2>
             <p>{business.address}</p>
             <p>{business.city}</p>
             <p>{business.zipCode}</p>
@@ -30,7 +30,7 @@ class Business extends React.Component {
           <div className="Business-reviews">
             <h3>{business.category}</h3>
             <h3 className="rating">{business.rating}</h3>
-            <p>{business.reviewCount}</p>
+            <p>{business.reviewCount} reviews</p>
           </div>
         </div>
       </div>
