3f11fb0e116863374077065c27ce744af6f1f963
viniciusrma <viniciusrma.adv@gmail.com>
2020-08-04 14:34:24 -0300

    fixed business component

--- src/components/business/Business.js
+++ src/components/business/Business.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import './Business.css';
 
-
+class Business extends React.Component {
+  render() {
+    return (
+      <div className="Business">
+        <div className="image-container">
+          <img src={this.props.business.imageSrc}  alt='' />
+        </div>
+        <div className="Business-information">
+          <div className="Business-address">
+            <h2>{this.props.business.name}</h2>
+            <p>{this.props.business.address}</p>
+            <p>{this.props.business.city}</p>
+            <p>{this.props.business.zipCode}</p>
+          </div>
+          <div className="Business-reviews">
+            <h3>{this.props.business.category}</h3>
+            <h3 className="rating">{business.rating}</h3>
+            <p>{this.props.business.reviewCount} reviews</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+};
 
 export default Business;
