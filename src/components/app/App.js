4cf678896defe0a503b99de9430a5f721b5ddaba
viniciusrma <viniciusrma.adv@gmail.com>
2020-08-05 10:50:57 -0300

    refactored all components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -24,14 +24,16 @@ const businesses = [
   business
 ];
 
-function App() {
-  return (
-    <div className="App">
-      <h1><img id="logo" src="https://i.ibb.co/zhtG3mx/Logo.png" alt="Logo of hunger of what"/>Hunger of What?!</h1>
-      <SearchBar />
-      <BusinessList businesses={businesses}/>
-    </div>
-  )
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1><img id="logo" src="https://i.ibb.co/zhtG3mx/Logo.png" alt="Logo of hunger of what"/>Hunger of What?!</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses}/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/components/businesslist/BusinessList.js
+++ src/components/businesslist/BusinessList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './BusinessList.css';
-import Business from '../app/App.js';
+import Business from '../business/Business';
 
 class BusinessList extends React.Component {
   render() {
