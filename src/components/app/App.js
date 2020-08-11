a27f401f21e1d9d5283941df9fa0ceed4060abd5
viniciusrma <viniciusrma.adv@gmail.com>
2020-08-10 21:03:10 -0300

    modify search yelp

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import './App.css';
 import BusinessList from '../businesslist/BusinessList';
 import SearchBar from '../searchbar/SearchBar';
-import Yelp from '../../util/Yelp'
+import Yelp from '../../util/Yelp';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp.bind(this);
+  }
+  
   searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
   }
 
   render() {
@@ -14,9 +26,9 @@ class App extends React.Component {
       <div className="App">
         <h1><img id="logo" src="https://i.ibb.co/zhtG3mx/Logo.png" alt="Logo of hunger of what"/>Hunger of What?!</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
-    )
+    );
   }
 }
 
